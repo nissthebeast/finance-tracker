87c7da99b65a0489d4b46315aa4a4059dee3a50e
nissthebeast <anissaisaac123@gmail.com>
2026-01-26 23:43:10 -0500

    Add GET by ID, PUT, and DELETE endpoints for transactions

---
+++ src/main/java/com/anissa/finance_tracker/controller/TransactionController.java
@@ -0,0 +1,42 @@
+package com.anissa.finance_tracker.controller;
+
+import com.anissa.finance_tracker.Transaction;
+import com.anissa.finance_tracker.TransactionRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/transactions")
+public class TransactionController {
+    private final TransactionRepository transactionRepository;
+
+    public TransactionController(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    @PostMapping
+    public Transaction createTransaction(@RequestBody Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @GetMapping
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    @GetMapping
+    public Transaction getTRansactionById(@RequestParam Long id) {
+        return transactionRepository.findById(id).get();
+    }
+
+    @PutMapping
+    public Transaction updateTransaction(@RequestBody Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    @DeleteMapping
+    public void deleteTransaction(@RequestBody Transaction transaction) {
+        transactionRepository.delete(transaction);
+    }
+}
