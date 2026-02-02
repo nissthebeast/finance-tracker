d6cb9c45652ba90582fd399ff82940d5157899e7
nissthebeast <anissaisaac123@gmail.com>
2026-02-01 23:30:34 -0500

    Add React frontend with transaction form and list display

--- src/main/java/com/anissa/finance_tracker/TransactionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.anissa.finance_tracker;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransactionRepository extends JpaRepository<Transaction, Long> {
-
-}
--- src/main/java/com/anissa/finance_tracker/TransactionType.java
+++
@@ -1,6 +0,0 @@
-package com.anissa.finance_tracker;
-
-public enum TransactionType {
-    INCOME,
-    EXPENSE
-}
